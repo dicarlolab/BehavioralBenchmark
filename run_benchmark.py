b0d013d44e186023d79403d1742934a3403d6fe6
ardila <ardila@mit.edu>
2014-11-05 14:55:37 -0500

    bugfix

--- run_benchmark.py
+++ run_benchmark.py
@@ -2,7 +2,8 @@ import os
 #from subprocess import call
 path = os.path.dirname(__file__)
 task_sets = ['hvm_basic_categorization']
-base_command = 'sbatch -n 10 -c 2 benchmark_test_set.sh '
+script_location = os.path.join(path, 'benchmark_task_set.sh')
+base_command = 'sbatch -n 10 -c 2 %s ' % script_location
 for task_set in task_sets:
     command = base_command+task_set
     os.system(command)
