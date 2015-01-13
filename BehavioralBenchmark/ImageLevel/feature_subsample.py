e806f676a1cf3fc6a3dae1939dddbaf1f40d1a7a
ardila <ardila@mit.edu>
2015-01-13 11:47:08 -0500

    Untested, but ready to run once feature_split is ready

--- BehavioralBenchmark/ImageLevel/feature_subsample.py
+++ BehavioralBenchmark/ImageLevel/feature_subsample.py
@@ -1,24 +1,31 @@
-from BehavioralBenchmark.ImageLevel import store_feature_results, feature_loader
+from BehavioralBenchmark.ImageLevel import store_feature_results, feature_loader, decoder_models
 
 __author__ = 'ardila'
 import sys
+import gridfs
+import pymongo as pm
 
+DB = pm.MongoClient(port=22334)['ModelBehavior']
 
 feature_name = sys.argv[1]
 
 feature_split = [int(ind) for ind in sys.argv[2].split(',')]
 
-# Code to load feature from name
+# Load feature from name
 
 features, meta = feature_loader.get_features_by_name(feature_name)
 
-#Todo: model name option, for now fixed
+# For now, this always uses the standard decoder model, but this can be reconfigured easily
 
+decoder_model = decoder_models.StandardModel
 
-decoder_model = None#Get model_config from ipython notebooks
+gridfs_name = '_'.join([decoder_model['name'], feature_name, 'results'])
 
-fs = None#Decide where to store things
+fs = gridfs.GridFS(DB, gridfs_name) # Decide where to store things
 
-additional_info = None#Decide on additional info
+additional_info = {'feature_split': feature_split}
 
-store_feature_results.store_subsampled_feature_results(features, meta, eval_config, fs, feature_split, additional_info)
+store_feature_results.store_subsampled_feature_results(features, meta,
+                                                       decoder_model, fs,
+                                                       feature_split,
+                                                       additional_info)
