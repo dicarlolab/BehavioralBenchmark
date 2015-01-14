70091fd7b163f9b7434e592ac7a2db6fa00d951a
ardila <ardila@mit.edu>
2015-01-14 14:19:29 -0500

    reconfigured for run on openmind

--- BehavioralBenchmark/ImageLevel/test_feature_split.py
+++ BehavioralBenchmark/ImageLevel/test_feature_split.py
@@ -9,10 +9,19 @@ class TestFeatureSplit(unittest.TestCase):
     """
 
     def test_feature_split(self):
-        answer = [[5, 2], [1, 3], [0, 4], [2, 3, 0], [5, 1, 4]]
-        test_value = feature_split(6, 2, 1)
-        print test_value
+        answer = [[5, 2], [1, 3], # 2 non overlapping of size 2
+                    [2, 3, 0], [5, 1, 4]] # 2 non overlapping of size 3
+        answer_with_bootstrap = [[5, 2], [1, 3], # 2 non overlapping of size 2
+                                 [2, 3], [0, 5], # 2 non overlapping of size 2
+                                    [5, 4, 3], [0, 1, 2], # 2 non overlapping of size 3
+                                    [2, 3, 0], [1, 4, 5]] # 2 non overlapping of size 3
+
+
+        test_value = feature_split(6, 2, 1, 2)
         self.assertItemsEqual(answer, test_value)
+        test_value = feature_split(6, 2, 2, 2)
+        print test_value
+        self.assertItemsEqual(answer_with_bootstrap, test_value)
 
 if __name__ == '__main__':
     unittest.main()
