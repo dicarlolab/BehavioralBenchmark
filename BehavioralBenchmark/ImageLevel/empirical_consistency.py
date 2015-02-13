11f8682018e404855eefca30f15f20d091961de6
Diego Ardila <ardila@mit.edu>
2015-02-13 16:57:19 -0500

    Uneccesary assertion removed

--- BehavioralBenchmark/ImageLevel/empirical_consistency.py
+++ BehavioralBenchmark/ImageLevel/empirical_consistency.py
@@ -109,7 +109,7 @@ def normalized_human_to_pool_consistency(human_individuals):
         consistencies_empirical = [metric(rep0_trials, rep0_pool), metric(rep1_trials, rep1_pool)]
         noise_level.append(np.sqrt(metric(rep0_pool, rep1_pool) * metric(rep0_trials, rep1_trials)))
 
-        assert rep0_pool.shape == (128,)
+        #assert rep0_pool.shape == (128,)
         individual_to_pool_empirical.extend(consistencies_empirical)
 
     normalized_humans = []
